1d2a095fca2dedabd4028f28b528e36149306517
Nicholas Cassleman <nicholascassleman@gmail.com>
2022-03-17 16:31:19 -0700

    Move list processing stuff into a module
    This might be premature
    
    lisp.js because list processing, getit?
    
    lists.js would make it sound like its used to process lists
    
    It's more about processing lists of lists
    
    The list is a kind of primitive flattenable container

---
+++ modules/lisp.js
@@ -0,0 +1,9 @@
+export const maybe = (truthy, array) => truthy ? array : [];
+
+export const first = (arrays) => arrays.find((x) => x.length > 0) || [];
+
+export const unzip = (entries) => {
+    const map = Object.fromEntries(entries);
+
+    return [ Object.keys(map), Object.values(map) ];
+};
