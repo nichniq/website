d31f0ab7d34f5dabe1fe07ca56cc0de1127b6c55
Nicholas Cassleman <nicholascassleman@gmail.com>
2022-03-17 16:00:09 -0700

    Allow non-node values for element children
    Convert non-node things into text and insert as a text node

--- scripts/html.js
+++ scripts/html.js
@@ -1,10 +1,14 @@
-export const text = (content) => [ document.createTextNode(content) ];
-
 export const element = (tag, content = [], attributes = {}) => {
     const element = document.createElement(tag);
 
-    for ( const child of content.flat(Infinity) ) {
-        element.appendChild(child);
+    for ( const child of [ content ].flat(Infinity) ) {
+        if (child instanceof Node) {
+            element.appendChild(child);
+        } else if (typeof child === "string") {
+            element.appendChild(document.createTextNode(child));
+        } else {
+            element.appendChild(document.createTextNode(JSON.stringify(child)));
+        }
     }
 
     for ( const [ name, value ] of Object.entries(attributes) ) {
