448e7c9420ca2fb0af56fb15748b8d785defae31
Nicholas Cassleman <nicholascassleman@gmail.com>
2022-04-24 16:42:01 -0700

    Contain the chaos
    I find this easier to read
    
    Did some more renaming + refactoring
    
    e.g. I made format a transform sequence

--- dsl/parser.js
+++ dsl/parser.js
@@ -1,15 +1,15 @@
-const breakpoint = value => { debugger; return value; };
-const extract_from_ = pattern => string => pattern.exec(string);
-const map_with_ = array => fn => array.map((x) => fn(x));
-const place_between_ = character => array => array.join(character);
-const search_for_else_ = array => match => value => array.find(match) ?? value;
-const split_on_ = string => character => string.split(character);
-const test_for_ = string => regex => regex.test(string);
-const pass_through_ = x => steps => steps.reduce((y, step) => step(y), x);
-const trimend = string => string.trimEnd();
-const wrap_around_ = ([ start, end ]) => string => `${start}${string}${end}`;
+const breakpoint = (value) => { debugger; return value; };
+const extract_from_ = (pattern, string) => pattern.exec(string);
+const apply_to_ = (fn, array) => array.map((x) => fn(x));
+const join_with_ = (array, character) => array.join(character);
+const search_for_else_ = (array, match, value) => array.find(match) ?? value;
+const split_on_ = (string, character) => string.split(character);
+const test_for_ = (string, regex) => regex.test(string);
+const pass_through_ = (x, steps) => steps.reduce((y, step) => step(y), x);
+const trimend_ = (string) => string.trimEnd();
+const wrap_around_ = ([ start, end ], string) => `${start}${string}${end}`;
 
-const type_ = line => search_for_else_(
+const type_ = (line) => search_for_else_(
   [
     [ "line_item",   /^- (.+)$/ ],          // "- Item"          -> "Item"
     [ "full_block",  /^{{ (.+) }}$/ ],      // "{{ text Text }}" -> "text Text"
@@ -18,40 +18,35 @@ const type_ = line => search_for_else_(
     [ "indented",    /^ {4}(.+)$/ ],        // "    indented"    -> "indented"
     [ "empty_line",  /^()$/ ],              // ""                -> ""
     [ "raw_text",    /^(.+)$/ ],            // "remaining"       -> "remaining"
-  ]
-)(
-  ([ type, pattern ]) => test_for_(line)(pattern)
-)(
-  [ "no_match", /^(.*)$/ ]
+  ],
+  ([ type, pattern ]) => test_for_(line, pattern),
+  [ "no_match", /^(.*)$/ ],
 );
 
-const label_ = (line) => pass_through_(
-  line
-)(
+const label_ = (line) => pass_through_(line,
   [
     (line)                         => [ line, type_(line) ],
-    ([ line, [ type, pattern ] ])  => [ type, extract_from_(pattern)(line) ],
+    ([ line, [ type, pattern ] ])  => [ type, extract_from_(pattern, line) ],
     ([ type, [ match, capture ] ]) => [ type, capture ],
   ]
 );
 
-const format_ = (lines) => wrap_around_(
-  [ "[", "]" ]
-)(
-  place_between_(
-    "\n"
-  )(
-    map_with_(lines)(([ type, content ]) => `[ "${type}", "${content}" ],`)
-  )
+const format_ = (lines) => pass_through_(lines,
+  [
+    (lines) => apply_to_(
+      ([ type, content ]) => `[ "${type}", "${content}" ],`,
+      lines
+    ),
+    (lines) => join_with_(lines, "\n"),
+    (output) => wrap_around_([ "[", "]" ], output),
+  ]
 );
 
-const parse = (raw) => pass_through_(
-  raw
-)(
+const parse = raw => pass_through_(raw,
   [
-    raw             => split_on_(raw)("\n"),
-    (lines)     => map_with_(lines)(trimend),
-    (lines) => map_with_(lines)(label_),
+    (raw)   => split_on_(raw, "\n"),
+    (lines) => apply_to_(trimend_, lines),
+    (lines) => apply_to_(label_, lines),
     (lines) => format_(lines),
   ]
 );
