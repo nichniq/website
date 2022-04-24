a11f4ca524e4580c60bb1ed5c99b15bc049f9772
Nicholas Cassleman <nicholascassleman@gmail.com>
2022-04-24 10:34:39 -0700

    Redefine parsing function as series of steps
    I feel like my `transform` function is pretty fancy
    
    I hope it's as clear to those as it is to me
    
    Love a list. Love many lists

--- dsl/parser.js
+++ dsl/parser.js
@@ -4,6 +4,7 @@ const match = (regex, string) => regex.exec(string);
 const for_all = (array, transform) => array.map((x) => transform(x));
 const trim_end = (string) => string.trimEnd();
 const join = (array, character) => array.join(character);
+const transform = (steps, input) => steps.reduce((x, step) => step(x), input);
 
 const line_types = [
   // example: "- List items start with a dash and space"
@@ -45,12 +46,23 @@ function label_line(line) {
   return [ "raw_text", line ];
 }
 
-function parse(raw) {
-  const raw_lines = split(raw, "\n");
-  const trimmed_lines = for_all(raw_lines, trim_end);
-  const labeled_lines  = for_all(trimmed_lines, label_line);
+const format_labeled_lines = (lines) => `[ ${join(
+  for_all(lines,
+    ([ type, content ]) => `[ "${type}", "${content}" ]`
+  ), "\n",
+)} ]`;
 
-  return labeled_lines;
+const parse = (raw) => transform([
+  (raw) => split(raw, "\n"),
+  (raw_lines) => for_all(raw_lines, trim_end),
+  (trimmed_lines) => for_all(trimmed_lines, label_line),
+  (labeled_lines) => format_labeled_lines(labeled_lines),
+ ], raw);
+
+const scroll = (mirror) => ({ target: source }) => {
+  if (mirror.scrollTop !== source.scrollTop) {
+    mirror.scroll({ top: source.scrollTop });
+  }
 }
 
 import raw from "/dsl/sample/00-raw.js";
@@ -58,22 +70,8 @@ import raw from "/dsl/sample/00-raw.js";
 const input = document.getElementById("input");
 const output = document.getElementById("output");
 
-const labeled_lines = parse(raw);
-
-const format_labeled_lines = (lines) => `[ ${join(
-  for_all(lines,
-    ([ type, content ]) => `[ "${type}", "${content}" ]`
-  ), "\n",
-)} ]`;
-
-input.value = format_labeled_lines(parse(raw));
+input.value = parse(raw);
 output.value = input.value;
-
-const scroll = (mirror) => ({ target: source }) => {
-  if (mirror.scrollTop !== source.scrollTop) {
-    mirror.scroll({ top: source.scrollTop });
-  }
-}
 
 input.addEventListener("scroll", scroll(output));
 output.addEventListener("scroll", scroll(input));
