33bd56e96dbda55cd291ddb1ca88fd13ff3ae086
Nicholas Cassleman <nicholascassleman@gmail.com>
2022-11-02 13:15:42 -0700

    Simplify hierarchy
    Turns out adding IDs made things complicated
    
    Don't need to save children info, it's already on the node

--- part-one/outline/scripts/hierarchy2.js
+++ part-one/outline/scripts/hierarchy2.js
@@ -1,58 +1,41 @@
 export default function hierarchy(root) {
-  const ids = new Map();
-  const nodes = {};
-  const parent_ids = {};
-  const children_ids = {};
+  const parents = new Map();
 
-  function injest_node(node, parent_id) {
-    const id = window.crypto.randomUUID();
+  function injest_node(node, parent) {
+    parents.set(node, parent);
 
-    ids.set(node, id);
-    nodes[id] = node;
-
-    parent_ids[id] = parent_id;
-
-    if (node.children == null) {
-      children_ids[id] = [];
-    } else {
-      for (const child of node.children || []) {
-        injest_node(child, id);
-        children_ids[id] = node.children.map(child => ids.get(child));
-      }
+    for (const child of node.children || []) {
+      injest_node(child, node);
     }
   }
 
-  function recursive_ancestors(output, id) {
-    return id == null
-      ? output
-      : recursive_ancestors([ ...output, id ], parent_ids[id]);
+  function recursive_ancestors(ancestors, node) {
+    return node == null
+      ? ancestors
+      : recursive_ancestors([ ...ancestors, node ], parents.get(node));
   }
 
-  function recursive_descendants(output, children = []) {
-    const next_gen = children.map(id => children_ids[id]).filter(x => x.length > 1);
+  function recursive_descendants(descendants, children) {
+    const next_gen = children.filter(c => c.children).map(c => c.children);
     return next_gen.length === 0
-      ? output
-      : recursive_descendants([ ...output, next_gen ], next_gen.flat());
+      ? descendants
+      : recursive_descendants([ ...descendants, next_gen ], next_gen.flat());
   }
 
   injest_node(root);
 
   return {
     get root() { return root },
-    get ids() { return ids },
-    get nodes() { return nodes },
-    get parent_ids() { return parent_ids },
-    get children_ids() { return children_ids },
+    get parents() { return parents },
 
-    id: node => ids.get(node),
-    node: id => nodes[id],
-    parent: id => parent_ids[id],
-    children: id => children_ids[id],
-
-    ancestors: id => recursive_ancestors([], parent_ids[id]),
-    descendants: id => recursive_descendants(
-      [ children_ids[id] || [] ],
-      children_ids[id]
+    ancestors: node => recursive_ancestors([], parents.get(node)),
+    descendants: node => recursive_descendants(
+      [ node.children || [] ],
+      node.children || []
     ),
+    nodes: () => recursive_descendants(
+      [ root.children ],
+      root.children
+    ).flat(Infinity),
   };
 }
