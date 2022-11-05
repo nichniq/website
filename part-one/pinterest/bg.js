5851ff97279de20cbed6b58697b5d191107e42d0
Nicholas Cassleman <nicholascassleman@gmail.com>
2022-11-05 04:42:04 -0700

    Get better messaging + logging, client <-> bg

--- part-one/pinterest/bg.js
+++ part-one/pinterest/bg.js
@@ -6,20 +6,37 @@ import { predictLength, downloadZip, makeZip } from "./third-party/client_zip/in
 /** DEFINITIONS **/
 
 const save = data => browser.storage.local.set(data);
-const load = keys => browser.storage.local.get(data);
-const log = (x, err) => { console[err === undefined ? 'log' : 'error'](x); return x; };
+const load = keys => browser.storage.local.get(keys);
 
-const responses = {
-  content_script_loaded({ last_loaded }) {
-    save({ last_loaded }).then(log);
-  },
+const send_to_tab = (tab_id, type, payload) => {
+  browser.tabs.sendMessage(tab_id, [ type, payload ]);
+  console.log(`bg sent: ${type}`, payload);
 };
 
 /** EXECUTION **/
 
-browser.runtime.onMessage.addListener(({ type, payload }) => {
-  if (type in responses) {
-    responses[type](payload);
+browser.runtime.onMessage.addListener(([ type, payload ], sender, sendResponse) => {
+  console.log(`bg received: ${type}`, payload);
+
+  switch(type) {
+    case "content_script_loaded":
+      const { last_loaded } = payload;
+
+      Promise.all([ load("last_loaded"), save({ last_loaded }) ]).then(([ saved, _ ]) => {
+        const diff_sec = Math.floor((last_loaded - saved.last_loaded) / 1000);
+
+        send_to_tab(sender.tab.id, "log_from_bg", [ "last_loaded", {
+          prev: saved.last_loaded,
+          curr: last_loaded,
+          diff: `${Math.floor(diff_sec / 60)}m ${Math.floor(diff_sec % 60)}s`,
+        } ]);
+      });
+
+      break;
+    default:
+      console.log("did nothing");
+
+      break;
   }
 });
 
--- part-one/pinterest/content.js
+++ part-one/pinterest/content.js
@@ -30,10 +30,25 @@ const pin_data = () => ({
 // script messaging
 
 const send_message = (type, payload) => {
+  browser.runtime.sendMessage([ type, payload ]);
   console.log("sent", type, payload);
-  browser.runtime.sendMessage({ type, payload });
 }
 
 /* EXECUTION */
 
 send_message("content_script_loaded", { last_loaded: Date.now() });
+
+browser.runtime.onMessage.addListener(([ type, payload ], sender, sendResponse) => {
+  console.log(`client received: ${type}`, payload);
+
+  switch (type) {
+    case "log_from_bg":
+      console.log(type, payload);
+
+      break;
+    default:
+      console.log("unknown event", type);
+
+      break;
+  }
+});
