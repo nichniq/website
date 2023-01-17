ca098c2adfdaff91c00aa3c1a444f456f012fa8a
Nicholas Cassleman <nicholascassleman@gmail.com>
2023-01-16 17:53:30 -0800

    Update Spotify scraper

--- seasonal-playlists/scrape-spotify.js
+++ seasonal-playlists/scrape-spotify.js
@@ -1,9 +1,15 @@
 /**
- * Go to Spotify
- * To make this a bookmarklet, save like the following:
+ * Go to a Spotify playlist and execute the following code to scrape the
+ * playlist and track information. I think at one point, I think the playlist
+ * track rows were loaded lazily, so I had to minimize the page to get all of
+ * them on the page. This doesn't seem to be the case anymore, but the script
+ * is including the suggested songs that come after the playlist, so I manually
+ * removed those.
+ *
+ * To make this a bookmarklet, save the following as a bookmark:
  *
  * javascript:(function(){})()
-*/
+ */
 
 const playlist_title = document.querySelector("h1").textContent;
 
@@ -31,33 +37,3 @@ const output = [
 
 /* 5. Copy the list to the clipboard */
 navigator.clipboard.writeText(output);
-
-javascript:(function() {
-  const playlist_title = document.querySelector("h1").textContent;
-
-  const playlist_id = document.querySelector(
-    '[data-testid="playlist-page"]'
-  ).dataset.testUri.split(":")[2];
-
-  const songs = [
-    ...document.querySelectorAll("[data-testid='tracklist-row']")
-  ].map(row => ({
-    title: row.querySelector("[dir='auto']").textContent,
-    artist: row.querySelector("[draggable=true]").textContent,
-  }));
-
-  const indent = (spaces, text) => " ".repeat(spaces) + text;
-
-  const output = [
-    `[ "${playlist_title}", {`,
-    `  ids: [ [ "spotify", "${playlist_id}" ] ],`,
-    `  tracks: [`,
-    ...songs.map(({ title, artist }) => `    { title: "${title}", artist: "${artist}" },`),
-    `  ]`,
-    `} ],`,
-  ].join("\n");
-
-  /* 5. Log output and copy to clipboard */
-  console.log(output);
-  navigator.clipboard.writeText(output);
-})()
